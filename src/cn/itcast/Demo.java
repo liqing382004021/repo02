87f244112df6e40230f338da0df63e18d50be8fd
zhangsan <zhangsan@itheima.com>
2018-12-04 21:39:23 +0800

    合作开发了项目更新了

--- src/cn/itcast/Demo.java
+++ src/cn/itcast/Demo.java
@@ -3,10 +3,12 @@ package cn.itcast;
 public class Demo {
     public static void main(String[] args) {
         System.out.println(10);
+        System.out.println("你好哦啊");
     }
 
     public void getId(){
+          int a = 100;
 
-        System.out.println(199999);
+        System.out.println(199999+a);
     }
 }
